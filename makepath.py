d7c78df87870d47f1f796972611c276f6c14779c
tkhabia <tanmay.khabia@students.iiit.ac.in>
2021-02-16 18:46:08 +0530

    code rrt complete but wrong line drawn

--- makepath.py
+++ makepath.py
@@ -1,3 +1,4 @@
+#%% 
 import numpy as np 
 import cv2 
 import matplotlib.pyplot as plt 
@@ -6,6 +7,10 @@ import matplotlib.pyplot as plt
 def makefloor(size , robosize): 
     floor = np.uint8( np.full (size , 255))
     # floor = cv2.copyMakeBorder(floor, 2,2,2,2,borderType=cv2.BORDER_CONSTANT)
+    floor[100 : 125 , 0:  500] = 0
+    floor[300 : 325 , -450 : ] = 0
+    # floor[]
+    
     if (robosize//2)&1:
         kernel = np.zeros((robosize//2,robosize//2,3))
         v = robosize//2 
@@ -15,8 +20,11 @@ def makefloor(size , robosize):
     kernel = np.uint8(cv2.circle(kernel ,(v//2,v//2) , 2 , (1, 0 , 0 ),thickness=-1 )[:,:,0])
     
     erosion = cv2.erode(floor,kernel,iterations = 1)
-    print(kernel)
     plt.imshow(erosion , cmap="gray")
     plt.show()
-    print(floor.shape)
-makefloor((600,600) , 10)
+    return erosion
+
+if __name__ == "__main__":
+    makefloor((600,600) , 10)
+
+# %%
--- rrt.py
+++ rrt.py
@@ -1,9 +1,10 @@
 import numpy as np
-import random
-import cv2 
+import math
+import cv2
+import matplotlib.pyplot as plt
 
+floorplan = np.uint8( np.full((600, 600, 3), 255))
 
-floorplan = np.full((600, 600 , 3) , 255)
 
 class rrttree:
     def __init__(self, x, y, nodeno, parent):
@@ -13,7 +14,7 @@ class rrttree:
         self.parent = parent
 
 
-N = 5000
+N = 500
 step_size = 10
 threshold = 25
 x_max = 600
@@ -23,8 +24,8 @@ bias = 0.2
 x_goal = 400
 y_goal = 500
 rrt = rrttree(5, 5, 1, 0)
-floorplan =cv2.circle(floorplan , (5, 5) , 4 , (0,255 , 0),-1)
-floorplan =cv2.circle(floorplan , (x_goal, y_goal) , 25 , (0,0 , 255),-1)
+floorplan = cv2.circle(floorplan, (5, 5), 4, (0, 255, 0), -1)
+floorplan = cv2.circle(floorplan, (x_goal, y_goal), 25, (0, 0, 255), -1)
 
 rrtT = {}
 rrtT[rrt.nodeno] = rrt
@@ -32,10 +33,19 @@ rrtT[rrt.nodeno] = rrt
 iter = 2
 
 
-def getdistance():
+def getdistance(rrtT , x_rand  , y_rand ):
     # return the min distance and the index for it.
-    # return distance  , parentind 
-    pass
+    # return distance  , parentind
+    min_dist=(y_max*x_max)+1
+    index=0
+    for i in range(len(rrtT)):
+        # tempnode=i
+        xt,yt= rrtT[i+1].x, rrtT[i+1].y
+        temp_dist=np.sqrt((x_rand-xt)**2 + (y_rand-yt)**2)
+        if temp_dist<min_dist:
+            min_dist=temp_dist
+            index=rrtT[i+1].nodeno
+    return min_dist, index
 
 
 def goalreached(rrtnode):
@@ -49,15 +59,33 @@ def goalreached(rrtnode):
         return False
 
 
-while iter < N:
-    if np.random.uniform(0, 1, 1)[0] < bias:
-        x_rand = x_goal
-        y_rand = y_goal
-    else:
-        x_rand = np.round(np.random.uniform(0, x_max))
-        y_rand = np.round(np.random.uniform(0, y_max))
+def drawline(rrt, parentind):
+    cv2.line(floorplan, (rrt.x, rrt.y),
+             (rrtT[parentind].x, rrtT[parentind].x), (255, 0, 0), 1)
+    cv2.circle(floorplan , (rrt.x, rrt.y) , 3 , (255 , 0 , 0 ) ,-1)
 
-    distance, parentind = getdistance()
+
+def checkobstical(x_new, y_new, floorplan, parentind):
+    x_old = rrtT[parentind].x
+    y_old = rrtT[parentind].y
+    inc = 1 if x_old - x_new > 0 else -1
+    for i in range(x_new, x_old, inc):
+        yl = (y_old - y_new) * (i - x_new) / (x_old - x_new)
+        # print(floorplan[i, int( math.ceil( yl))])
+        if floorplan[i, int( math.ceil( yl))][0] == 0 or floorplan[i, int( math.floor( yl))][0] == 0:
+            return True
+    return False
+
+
+while iter < N:
+    # if np.random.uniform(0, 1, 1)[0] < bias:
+    #     x_rand = x_goal
+    #     y_rand = y_goal
+    # else:
+    x_rand = np.round(np.random.uniform(0, x_max))
+    y_rand = np.round(np.random.uniform(0, y_max))
+
+    distance, parentind = getdistance(rrtT , x_rand , y_rand )
 
     if distance >= step_size:
         x_new = rrtT[parentind].x + \
@@ -67,10 +95,19 @@ while iter < N:
     else:
         x_new = x_rand
         y_new = y_rand
-    
+
+    x_new = np.int(np.round(x_new))
+    y_new = np.int(np.round(y_new))
+
+    if checkobstical(x_new, y_new, floorplan, parentind):
+        continue
+
     rrt = rrttree(x_new, y_new, iter, parentind)
     rrtT[rrt.nodeno] = rrt
-
-    if goalreached(rrt ):
+    drawline(rrt, parentind)
+    if goalreached(rrt):
         break
     iter += 1
+
+plt.imshow(floorplan)
+plt.show()
